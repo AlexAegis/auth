ce20e3c15071a15df44c8706a485f6811226afc1
AlexAegis <alexaegis@gmail.com>
2020-05-20 01:01:31 +0200

    ci: added more extensive linting

--- apps/core-demo/jest.config.js
+++ apps/core-demo/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'core-demo',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/core-demo',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
-  ],
+	name: 'core-demo',
+	preset: '../../jest.config.js',
+	coverageDirectory: '../../coverage/apps/core-demo',
+	snapshotSerializers: [
+		'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+		'jest-preset-angular/build/AngularSnapshotSerializer.js',
+		'jest-preset-angular/build/HTMLCommentSerializer.js',
+	],
 };
