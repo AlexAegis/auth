93cb0de8f8d78982b1a2a257c24b4b75d5b243c2
AlexAegis <alexaegis@gmail.com>
2020-05-24 00:53:43 +0200

    feat(token): token model

---
+++ libs/toolbox/src/lib/jwt-token.class.spec.ts
@@ -0,0 +1,24 @@
+import { JwtToken } from './jwt-token.class';
+
+describe('JwtToken', () => {
+	const validHeader = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+	const validPayload =
+		'eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ';
+	const validSignature = 'SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+	const validToken = [validHeader, validPayload, validSignature].join(
+		JwtToken.JWT_TOKEN_SEPARATOR
+	);
+
+	const invalidToken = [validHeader, validPayload].join(JwtToken.JWT_TOKEN_SEPARATOR);
+
+	it('can be made from a valid token string', () => {
+		const result = JwtToken.from(validToken);
+
+		expect(result).toBeInstanceOf(JwtToken);
+	});
+	it("can't be made from a valid token string", () => {
+		const result = JwtToken.from(invalidToken);
+
+		expect(result).toBeNull();
+	});
+});
