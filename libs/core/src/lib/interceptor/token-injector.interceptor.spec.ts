aff2ab40dc25b2cf40c00de57bc3fa9095842315
AlexAegis <alexaegis@gmail.com>
2020-06-01 22:28:00 +0200

    fix(core): token injector test providers

--- libs/core/src/lib/interceptor/token-injector.interceptor.spec.ts
+++ libs/core/src/lib/interceptor/token-injector.interceptor.spec.ts
@@ -1,10 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { AuthConfiguration, TypedValueProvider } from '../model';
+import { AuthCoreModuleConfigurationService } from '../token';
 import { TokenInjectorInterceptor } from './token-injector.interceptor';
 
 describe('TokenInjectorInterceptor', () => {
 	beforeEach(() =>
 		TestBed.configureTestingModule({
-			providers: [TokenInjectorInterceptor],
+			imports: [],
+			providers: [
+				TokenInjectorInterceptor,
+				{
+					provide: AuthCoreModuleConfigurationService,
+					useValue: {
+						getToken: () => 'fakeToken',
+					},
+				} as TypedValueProvider<AuthConfiguration>,
+			],
 		})
 	);
 
