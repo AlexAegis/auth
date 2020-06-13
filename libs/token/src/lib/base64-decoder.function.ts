1521028476fe385783eb035f79a8ca9abac29776
AlexAegis <alexaegis@gmail.com>
2020-06-13 22:45:38 +0200

    test: increased coverage

--- libs/token/src/lib/base64-decoder.function.ts
+++ libs/token/src/lib/base64-decoder.function.ts
@@ -3,5 +3,15 @@ import { Base64 } from 'js-base64';
 export type Base64String = string;
 export type JsonString = string;
 
-export const decodeJsonLikeBase64 = <T = Record<string, unknown>>(str: Base64String): T =>
-	JSON.parse(Base64.decode(str));
+/**
+ *
+ * @param str json encoded in Base64
+ */
+export const decodeJsonLikeBase64 = <T = Record<string, unknown>>(str: Base64String): T | null => {
+	try {
+		return JSON.parse(Base64.decode(str));
+	} catch (error) {
+		console.error('Invalid Jsonlike Base64 string', error);
+		return null;
+	}
+};
