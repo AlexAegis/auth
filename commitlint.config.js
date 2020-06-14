4492c03e10ed5ac31cc6b89820f2c32185ef1d5c
AlexAegis <alexaegis@gmail.com>
2020-06-15 00:38:11 +0200

    chore(lint): added release as a commit scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,11 @@
 module.exports = {
 	extends: ['@commitlint/config-conventional'],
-	type: ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
+	type: ['feat', 'fix', 'test', 'docs', 'refactor', 'build', 'ci', 'perf', 'revert', 'style'],
 	rules: {
-		'scope-enum': [2, 'always', ['core', 'jwt', 'package', 'docs', 'lint', 'changelog']],
+		'scope-enum': [
+			2,
+			'always',
+			['core', 'jwt', 'package', 'docs', 'lint', 'changelog', 'release'],
+		],
 	},
 };
