16876a8a317e3c5d084815e132b63b325eb6d5be
AlexAegis <alexaegis@gmail.com>
2020-06-28 19:23:31 +0200

    test(jwt): enabled coverage for jwt, module test

--- libs/jwt/src/lib/jwt.module.spec.ts
+++ libs/jwt/src/lib/jwt.module.spec.ts
@@ -1,14 +1,22 @@
-import { async, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { JwtModule } from './jwt.module';
+import { JWT_REFRESH_CONFIGURATION_TOKEN } from './token';
 
 describe('JwtModule', () => {
-	beforeEach(async(() => {
+	it('should create without refresh config', () => {
 		TestBed.configureTestingModule({
-			imports: [JwtModule],
+			imports: [JwtModule.forRoot({ useValue: {} })],
 		}).compileComponents();
-	}));
-
-	it('should create', () => {
 		expect(JwtModule).toBeDefined();
+		expect(() => TestBed.inject(JWT_REFRESH_CONFIGURATION_TOKEN)).toThrow();
+	});
+
+	it('should create with refresh config', () => {
+		TestBed.configureTestingModule({
+			imports: [JwtModule.forRoot({ useValue: {} }, { useValue: {} })],
+		}).compileComponents();
+		expect(JwtModule).toBeDefined();
+		const refreshConfig = TestBed.inject(JWT_REFRESH_CONFIGURATION_TOKEN);
+		expect(refreshConfig).toBeTruthy();
 	});
 });
