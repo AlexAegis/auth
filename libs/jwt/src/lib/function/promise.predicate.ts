3ac50152f422c88f83bed563f001ebb66d04b899
AlexAegis <alexaegis@gmail.com>
2020-06-28 20:02:54 +0200

    test(jwt): promise predicate tested

--- libs/jwt/src/lib/function/promise.predicate.ts
+++ libs/jwt/src/lib/function/promise.predicate.ts
@@ -1,3 +1,12 @@
+/**
+ * Returns true if the object is truthy and has a `then` and a `catch` function.
+ * Using `instanceof` would not be sufficient as Promises can be contructed
+ * in many ways, and it's just a specification.
+ */
 export function isPromise<T>(promiseLike: unknown): promiseLike is Promise<T> {
-	return promiseLike && typeof (promiseLike as Promise<T>).then === 'function';
+	return (
+		promiseLike &&
+		typeof (promiseLike as Promise<T>).then === 'function' &&
+		typeof (promiseLike as Promise<T>).catch === 'function'
+	);
 }
