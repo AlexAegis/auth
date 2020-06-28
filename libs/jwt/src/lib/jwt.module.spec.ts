f0337b0d8be3a6ba27886c2e907af891c9225e0e
AlexAegis <alexaegis@gmail.com>
2020-06-28 19:27:06 +0200

    test(jwt): module test extended

--- libs/jwt/src/lib/jwt.module.spec.ts
+++ libs/jwt/src/lib/jwt.module.spec.ts
@@ -1,6 +1,8 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { JwtInjectorInterceptor, JwtRefreshInterceptor } from './interceptor';
 import { JwtModule } from './jwt.module';
-import { JWT_REFRESH_CONFIGURATION_TOKEN } from './token';
+import { JWT_CONFIGURATION_TOKEN, JWT_REFRESH_CONFIGURATION_TOKEN } from './token';
 
 describe('JwtModule', () => {
 	it('should create without refresh config', () => {
@@ -8,6 +10,13 @@ describe('JwtModule', () => {
 			imports: [JwtModule.forRoot({ useValue: {} })],
 		}).compileComponents();
 		expect(JwtModule).toBeDefined();
+		const jwtConfig = TestBed.inject(JWT_CONFIGURATION_TOKEN);
+		expect(jwtConfig).toBeTruthy();
+		const interceptors = TestBed.inject(HTTP_INTERCEPTORS);
+		const jwtInterceptor = interceptors.find((i) => i instanceof JwtInjectorInterceptor);
+		expect(jwtInterceptor).toBeTruthy();
+		const jwtRefreshInterceptor = interceptors.find((i) => i instanceof JwtRefreshInterceptor);
+		expect(jwtRefreshInterceptor).toBeFalsy();
 		expect(() => TestBed.inject(JWT_REFRESH_CONFIGURATION_TOKEN)).toThrow();
 	});
 
@@ -16,7 +25,18 @@ describe('JwtModule', () => {
 			imports: [JwtModule.forRoot({ useValue: {} }, { useValue: {} })],
 		}).compileComponents();
 		expect(JwtModule).toBeDefined();
+		const jwtConfig = TestBed.inject(JWT_CONFIGURATION_TOKEN);
+		expect(jwtConfig).toBeTruthy();
+		const interceptors = TestBed.inject(HTTP_INTERCEPTORS);
+		const jwtInterceptor = interceptors.find((i) => i instanceof JwtInjectorInterceptor);
+		expect(jwtInterceptor).toBeTruthy();
+		const jwtRefreshInterceptor = interceptors.find((i) => i instanceof JwtRefreshInterceptor);
+		expect(jwtRefreshInterceptor).toBeTruthy();
 		const refreshConfig = TestBed.inject(JWT_REFRESH_CONFIGURATION_TOKEN);
 		expect(refreshConfig).toBeTruthy();
+	});
+
+	afterEach(() => {
+		TestBed.resetTestingModule();
 	});
 });
