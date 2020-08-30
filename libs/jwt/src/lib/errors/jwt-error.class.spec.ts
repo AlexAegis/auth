632cc3fffc51e0d51283167349e621eb78474c17
AlexAegis <alexaegis@gmail.com>
2020-08-30 17:04:37 +0200

    test(jwt): error class tests

--- libs/jwt/src/lib/errors/jwt-error.class.spec.ts
+++ libs/jwt/src/lib/errors/jwt-error.class.spec.ts
@@ -1,3 +1,20 @@
+import { HttpRequest } from '@angular/common/http';
+import { JwtCannotRefreshError, JwtCouldntRefreshError, JwtError } from './jwt-error.class';
+
 describe('JwtError', () => {
-	test.todo('should be creatable');
+	it('should be creatable', () => {
+		expect(new JwtError(new HttpRequest('GET', ''), '')).toBeTruthy();
+	});
+});
+
+describe('JwtCannotRefreshError', () => {
+	it('should be creatable', () => {
+		expect(new JwtCannotRefreshError(new HttpRequest('GET', ''), '')).toBeTruthy();
+	});
+});
+
+describe('JwtCouldntRefreshError', () => {
+	it('should be creatable', () => {
+		expect(new JwtCouldntRefreshError(new HttpRequest('GET', ''), '')).toBeTruthy();
+	});
 });
