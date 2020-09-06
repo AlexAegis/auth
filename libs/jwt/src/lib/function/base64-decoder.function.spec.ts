c302a3f6c22a07af9542892608d78a07fc9088c4
AlexAegis <alexaegis@gmail.com>
2020-09-07 00:30:17 +0200

    test(jwt): try and do refresh tests

--- libs/jwt/src/lib/function/base64-decoder.function.spec.ts
+++ libs/jwt/src/lib/function/base64-decoder.function.spec.ts
@@ -1,10 +1,15 @@
 import { decodeJsonLikeBase64 } from './base64-decoder.function';
 
-describe('decode', () => {
+describe('decodeJsonLikeBase64', () => {
 	it('can decode an object', () => {
-		// "{ "hello": "world" }"
 		const result = decodeJsonLikeBase64<{ hello: 'world' }>('eyJoZWxsbyI6ICJ3b3JsZCJ9');
-
 		expect(result?.hello).toEqual('world');
+	});
+
+	it('can not decode garbage, logs the error and returns null', () => {
+		console.error = jest.fn();
+		const result = decodeJsonLikeBase64<unknown>('foo');
+		expect(console.error).toHaveBeenCalledTimes(1);
+		expect(result).toBeNull();
 	});
 });
