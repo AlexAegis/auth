5cf5bc554f19eb8d23faed60992a02da59c2c5c9
AlexAegis <alexaegis@gmail.com>
2020-09-06 23:45:19 +0200

    test(jwt): error handling tests

---
+++ libs/jwt/src/lib/function/handle-jwt-failure.function.ts
@@ -0,0 +1,39 @@
+import { HttpParams } from '@angular/common/http';
+import { Params, Router } from '@angular/router';
+import { isString } from '../function/string.predicate';
+
+/**
+ * Jwt failures are handled by either calling a callback or if its a string,
+ * redirect
+ *
+ * @internal
+ */
+export function handleJwtFailure<E>(
+	errorCallbackOrRedirect: string | ((error: E) => void),
+	error: E,
+	router?: Router,
+	redirectParameters?: ((error: E) => HttpParams | Params) | HttpParams | Params
+): void {
+	if (isString(errorCallbackOrRedirect)) {
+		if (router) {
+			let queryParams = redirectParameters;
+			if (typeof redirectParameters === 'function') {
+				queryParams = redirectParameters(error);
+			}
+
+			router.navigate([errorCallbackOrRedirect], {
+				queryParams,
+			});
+		} else {
+			// This error is intended to surface as it's a configuration problem
+			throw new Error(
+				'JWT Refresh configuration error! ' +
+					'`onFailure` is defined as a string, but the ' +
+					'Router is not available! Is @angular/router ' +
+					'installed and the RouterModule imported?'
+			);
+		}
+	} else {
+		errorCallbackOrRedirect(error);
+	}
+}
