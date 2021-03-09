4921ecdf000cde681ebcf904ced0d542c9622324
AlexAegis <alexaegis@gmail.com>
2021-03-09 08:25:51 +0100

    test: fixed e2e tests

--- apps/jwt-demo-e2e/src/integration/app.spec.ts
+++ apps/jwt-demo-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('jwt-demo', () => {
 		cy.login('my-email@something.com', 'myPassword');
 
 		// Function helper example, see `../support/app.po.ts` file
-		getGreeting().contains('Welcome to jwt-demo!');
+		getGreeting().contains('jwt-demo');
 	});
 });
