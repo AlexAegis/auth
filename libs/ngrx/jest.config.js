9df48c4953d8a013a666f1ce8b47865880ef39f3
AlexAegis <alexaegis@gmail.com>
2022-05-02 21:32:01 +0200

    chore: applied nx 12 migrations

--- libs/ngrx/jest.config.js
+++ libs/ngrx/jest.config.js
@@ -2,22 +2,18 @@ module.exports = {
 	name: 'ngrx',
 	preset: '../../jest.preset.js',
 	coverageDirectory: '../../coverage/libs/ngrx',
-	snapshotSerializers: [
-		'jest-preset-angular/build/serializers/no-ng-attributes',
-		'jest-preset-angular/build/serializers/ng-snapshot',
-		'jest-preset-angular/build/serializers/html-comment',
-	],
+
 	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
 	globals: {
 		'ts-jest': {
 			tsconfig: '<rootDir>/tsconfig.spec.json',
 			stringifyContentPathRegex: '\\.(html|svg)$',
-			astTransformers: {
-				before: [
-					'jest-preset-angular/build/InlineFilesTransformer',
-					'jest-preset-angular/build/StripStylesTransformer',
-				],
-			},
 		},
 	},
+	snapshotSerializers: [
+		'jest-preset-angular/build/serializers/no-ng-attributes',
+		'jest-preset-angular/build/serializers/ng-snapshot',
+		'jest-preset-angular/build/serializers/html-comment',
+	],
+	transform: { '^.+\\.(ts|js|html)$': 'jest-preset-angular' },
 };
