0cad5e4587eab8285c656370329454fc0a6f96bc
AlexAegis <alexaegis@gmail.com>
2022-05-02 23:41:51 +0200

    fix: stylelint migration

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@
 module.exports = {
 	'*.(ts|js)': ['eslint --max-warnings=0 --no-ignore', 'prettier --check'],
 	'*.css': ['stylelint', 'prettier --list-different'],
-	'*.scss': ['stylelint --syntax=scss', 'prettier --check'],
+	'*.scss': ['stylelint --customSyntax=postcss-scss', 'prettier --check'],
 	'(*.json|.eslintrc|.prettierrc|.stylelintrc|.markdownlintrc)': [('eslint', 'prettier --check')],
 	'*.md': ["markdownlint --ignore 'CHANGELOG.md' --ignore-path '.gitignore'", 'prettier --check'],
 	'*.(yml|yaml)': ['prettier --check'],
