261e2897bdc17b2b576d15a43bbf36972090beea
AlexAegis <alexaegis@gmail.com>
2022-05-02 23:07:39 +0200

    chore: ran nx 13 migrations

--- apps/jwt-demo/jest.config.ts
+++ apps/jwt-demo/jest.config.ts
@@ -1,7 +1,7 @@
 module.exports = {
-	name: 'ngrx',
-	preset: '../../jest.preset.js',
-	coverageDirectory: '../../coverage/libs/ngrx',
+	name: 'jwt-demo',
+
+	coverageDirectory: '../../coverage/apps/jwt-demo',
 
 	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
 	globals: {
@@ -15,5 +15,9 @@ module.exports = {
 		'jest-preset-angular/build/serializers/ng-snapshot',
 		'jest-preset-angular/build/serializers/html-comment',
 	],
-	transform: { '^.+\\.(ts|js|html)$': 'jest-preset-angular' },
+	transform: {
+		'^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
+	},
+	transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
+	preset: '../../jest.preset.ts',
 };
--- jest.preset.ts
+++ jest.preset.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const nxPreset = require('@nrwl/jest/preset');
 
 module.exports = { ...nxPreset, coverageReporters: ['html', 'json', 'lcov', 'cobertura'] };
