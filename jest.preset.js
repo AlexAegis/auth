517e9326b6f8fcdc9198634f22380cbb2d4c72b9
Győri Sándor <alexaegis@gmail.com>
2022-07-01 09:13:28 +0200

    feat: updated to ng14 (#552)

--- jest.preset.js
+++ jest.preset.js
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const nxPreset = require('@nrwl/jest/preset');
+const nxPreset = require('@nrwl/jest/preset').default;
 
 module.exports = { ...nxPreset, coverageReporters: ['html', 'json', 'lcov', 'cobertura'] };
