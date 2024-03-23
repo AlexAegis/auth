4541be4bc480aabee7c9799286483d8608ffae54
AlexAegis <alexaegis@pm.me>
2024-03-23 20:40:23 +0100

    feat: ng16

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { getJestProjects } = require('@nrwl/jest');
+const { getJestProjects } = require('@nx/jest');
 
 export default {
 	testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
 	transform: {
 		'^.+\\.(ts|js|html)$': 'ts-jest',
 	},
-	resolver: '@nrwl/jest/plugins/resolver',
+	resolver: '@nx/jest/plugins/resolver',
 	moduleFileExtensions: ['ts', 'js', 'html'],
 	coverageReporters: ['html', 'json', 'lcov', 'cobertura'],
 	projects: getJestProjects(),
